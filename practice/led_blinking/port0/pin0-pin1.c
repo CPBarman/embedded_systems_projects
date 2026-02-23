0ead072385f7bef3d666de946fbaf7221e1a5be2
Chaitonna Prosad Barman <155560450+CPBarman@users.noreply.github.com>
2026-02-23 23:54:52 +0530

    Implement LED blinking on Port 0 Pins 0 and 1
    Added code to blink two LEDs connected to Port 0 Pins 0 and 1.

--- practice/led_blinking/port0/pin0-pin1.c
+++ practice/led_blinking/port0/pin0-pin1.c
@@ -1,1 +1,15 @@
+//Using Port 0 Pins 0 and 1 to turn ON and OFF repeatedly
+#include<reg51.h>
+sbit LED1=P0^0;
+sbit LED2=P0^1;
+void main()
+{
+    while(1)
+    {
+        LED1=0;
+        LED1=1;
 
+        LED2=0;
+        LED2=1;
+    }
+}
